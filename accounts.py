fe0f9aff29d694a877da43b98b60c8eda7a08866
KaysanMR <127319321+KaysanMR@users.noreply.github.com>
2024-02-01 11:25:29 +0000

    replace recursion with while loop

--- accounts.py
+++ accounts.py
@@ -1,18 +1,29 @@
-def create():
-  username = input("Enter your username:")
-  password = input("Enter your password:")
-  if input("Confirm password: ") ==  password:
-    print("Account created")
-    return [username, password]
-  else:
-    print("Passwords do not match")
-    return create()
 
+import random
+import math
+id_list = []
+
+def generate_id():
+  id = math.floor(random.random() * 1006)
+  id_list.append(id) if id not in id_list else generate_id()
+  return id
+  
+def create():
+  while True:
+    username = input("Enter your username:")
+    password = input("Enter your password:")
+    if input("Confirm password: ") ==  password:
+      print("Account created")
+      return [username, password]
+    else:
+      print("Passwords do not match.")
+      break
+  
 def store(credentials):
   with open("accounts.txt", "a") as file:
-    file.write(', '.join(credentials) + '\n')
-
-
-store(create())
-
+    file.write(f"[{generate_id()}, {', '.join(credentials)}]" + '\n')
   
+  
+[store(create()) for i in range(3)]
+
+
