eac4d62388f72e1f5c64d7ec0f6b5c45fca0af8f
KaysanMR <127319321+KaysanMR@users.noreply.github.com>
2024-02-02 04:24:03 +0000

    add placeholder ID function

--- accounts.py
+++ accounts.py
@@ -1,12 +1,10 @@
-
-  
 def new_user():
   while True:
     username = input("Enter your username:")
     password = input("Enter your password:")
     if input("Confirm password: ") ==  password:
       print("Account created")
-      write(f"{username}, {password}" + '\n')
+      write(f"{new_id()}, {username}, {password}" + '\n')
       break
     else:
       print("Passwords do not match.")
@@ -24,7 +22,10 @@ def load_users(filename):
   with open(filename, "r") as file:
     content = file.readlines()
     return [line.strip().split(',') for line in content]
-    
-    
+
+
 userlist = load_users("accounts.txt")
 print(userlist[0][0])
+
+def new_id():
+  return "ABC123"
