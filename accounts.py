fcf2e1e523fd57ddde7fa7344d8eaf991e04e85c
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2024-02-02 17:31:34 +0800

    restructure file for pycharm

--- accounts.py
+++ accounts.py
@@ -1,25 +1,25 @@
 def new_user():
-  while True:
-    username = input("Enter your username:")
-    password = input("Enter your password:")
-    if input("Confirm password: ") ==  password:
-      print("Account created")
-      write(f"{new_id()}, {username}, {password}" + '\n')
-      break
-    else:
-      print("Passwords do not match.")
+    while True:
+        username = input("Enter your username:")
+        password = input("Enter your password:")
+        if input("Confirm password: ") == password:
+            print("Account created")
+            write(f"{new_id()}, {username}, {password}" + '\n')
+            break
+        else:
+            print("Passwords do not match.")
 
 
 def write(text):
-  with open("accounts.txt", "a") as file:
-    file.write(text)
+    with open("accounts.txt", "a") as file:
+        file.write(text)
 
 
 def load_users(filename):
-  with open(filename, "r") as file:
-    content = file.readlines()
-    return [line.strip().split(',') for line in content]
+    with open(filename, "r") as file:
+        content = file.readlines()
+        return [line.strip().split(',') for line in content]
 
 
 def new_id():
-  return "ABC123"
+    return "ABC123"
--- main.py
+++ main.py
@@ -2,31 +2,33 @@ import accounts
 
 userlist = accounts.load_users("accounts.txt")
 
+
 def menu():
-  while True:
-    print("\n")
-    print("1. Sign in")
-    print("2. Create account")
-    print("3. Exit")
-    choice = int(input("Enter your choice: \n> "))
+    while True:
+        print("\n")
+        print("1. Sign in")
+        print("2. Create account")
+        print("3. Exit")
+        choice = int(input("Enter your choice: \n> "))
 
-    match choice:
-      case 1:
-        print("sign in")
-      case 2:
-        print("\nCREATE ACCOUNT")
-        accounts.new_user()
-      case 3:
-        match input("\nAre you sure you want to exit? (y/n) \n> "):
-          case "y":
-            break
-          case "n": 
-            continue
-          case _:
-            print("Invalid choice")          
-      case _:
-        print("Invalid choice")  
+        match choice:
+            case 1:
+                print("sign in")
+            case 2:
+                print("\nCREATE ACCOUNT")
+                accounts.new_user()
+            case 3:
+                match input("\nAre you sure you want to exit? (y/n) \n> "):
+                    case "y":
+                        break
+                    case "n":
+                        continue
+                    case _:
+                        print("Invalid choice")
+            case _:
+                print("Invalid choice")
+
+            # menu()
 
 
-# menu()
 print(userlist[0])
