c7771068e942d2fd9198e5fb21b88f4144444f14
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2024-02-09 19:58:00 +0800

    test creation of admin accounts

--- accounts.py
+++ accounts.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 
 
-def new_user(data):
+def new_user(data, admin=False):
     username = None
     while not username:
         username = input("Enter username:")
         for user in data:
-            if username == user[1]:
-                print("Username already exists, please try again.")
-                username = None
+            try:
+                if username == user[1]:
+                    print("Username already exists, please try again.")
+                    username = None
+            except IndexError:
+                continue
 
-    uid = new_id(username)
+    uid = new_id(username, admin)
     password = username[:3] + uid[8:]
 
     data.append([uid, username, password])
-    save(data)
+    save(data, "accounts.txt") if not admin else save(data, "admin.txt")
     print("Account created")
 
 
@@ -37,14 +40,16 @@ def load_users(filename):
         return data
 
 
-def new_id(username):
+def new_id(username, admin=False):
     timestamp = round(datetime.timestamp(datetime.now()))
     identifier = username[:3].upper() + str(timestamp)
+    if admin:
+        identifier = identifier + "A"
     return identifier
 
 
-def save(data):
-    with open("accounts.txt", "w") as file:
+def save(data, file="accounts.txt"):
+    with open(file, "w") as file:
         for user in data:
             file.write(repr([item for item in user]) + '\n')
 
@@ -69,6 +74,6 @@ def login(data):
 
 if __name__ == "__main__":
     userlist = load_users("accounts.txt")
-    print(userlist)
-    current_user = login(userlist)
-    set_pass(current_user, userlist)
+    admins = load_users("admin.txt")
+    print(admins)
+    new_user(userlist)
