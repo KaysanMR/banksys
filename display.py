3cfbd9902830fa14f810f2e31c68cfd4de7c83a7
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2024-02-20 23:29:41 +0800

    clean up display.py

--- display.py
+++ display.py
@@ -3,27 +3,28 @@ import accounts
 
 
 def current_time():
-  return datetime.now().strftime("%H:%M:%S %Y-%m-%d")  # Format time to time, date format
+    return datetime.now().strftime("%H:%M:%S %Y-%m-%d")  # Format time to time, date format
 
 
 def greet(user=None):
-  if user:
-    print(f"Welcome, {user[1]}! It is currently {current_time()}")
-  else:
-    print(f"Welcome! It is currently {current_time()}")
+    if user:
+        print(f"Welcome, {user[1]}! It is currently {current_time()}")
+    else:
+        print(f"Welcome! It is currently {current_time()}")
 
 
 def table(data, headings, cell_width=15):
-  cell = str("{:^" + str(cell_width) + "}￨")
-  format_row = "￨{:^3}￨" + cell * (len(headings))
+    cell = str("{:^" + str(cell_width) + "}￨")
+    format_row = "￨{:^3}￨" + cell * (len(headings))
+    divider_line = ("---",) + (("-" * cell_width),) * len(headings)
 
-  print(format_row.format("#", *headings))
-  print(format_row.format(*("---",) + (("-" * cell_width),) * len(headings)))
-  for index, item in enumerate(data):
-    print(format_row.format(index + 1, *item))
+    print(format_row.format("#", *headings))
+    print(format_row.format(*divider_line))
+    for index, item in enumerate(data):
+        print(format_row.format(index + 1, *item))
 
 
 if __name__ == "__main__":
-  users = accounts.load("accounts.txt")
-  headers = ["UID", "USERNAME"]
-  table(users, headers)
+    users = accounts.load("accounts.txt")
+    headers = ["UID", "USERNAME"]
+    table(users, headers)
