b821f1692a4f44c79b6193324edcc7ebd9853802
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2024-02-23 16:28:52 +0800

    remove legacy logging function

--- logger.py
+++ logger.py
@@ -2,28 +2,10 @@ from file_manager import write
 from display import current_time
 
 
-def log_function(function):
-
-    def wrapper(*args, **kwargs):
-        result = function(*args, **kwargs)
-        log = f"{current_time(2)}: {function.__name__} returned: {result}\n"
-        write(log, "log.txt")
-        return result
-
-    return wrapper
-
-
 def log_entry(action, session="SYSTEM"):
     log = f"{current_time(2)}: [{session}] {action}\n"
     write(log, "log.txt")
 
 
 if __name__ == "__main__":
-    def hello(a=1, b=2, word="world"):
-        print("Hello " + word)
-        return a + b
-
-
-    session_user = "ADM1707479281A"
-    hello(a=15, word="Khalid")
-    hello(word="Bakyt")
+    pass
